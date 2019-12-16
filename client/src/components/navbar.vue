401c9e062a0fca2f9f3b7c557d1f9f24fa02b585
SYX <Syirilrakhulhaqim@gmail.com>
2019-12-16 09:06:29 +0700

    kanban done

---
+++ client/src/components/navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" class="fixed-top" type="dark" variant="info">
+      <b-navbar-brand href="#">Kanban</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <!--  -->
+          <b-navbar-nav>
+            <b-nav-item href v-b-modal.modal>
+              <i class="fa fa-plus-square"></i>
+              Add Todo
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <Modal></Modal>
+  </div>
+</template>
+
+<script>
+import Modal from "../components/modals";
+export default {
+  components: {
+    Modal
+  }
+};
+</script>
+
+<style></style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
